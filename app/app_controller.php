37e2f4059e8d955b03ca5845f234e15fc2c0e578
Ernesto <sirlancecc@hotmail.com>
2011-06-17 14:51:59 -0500

    usuarios cambios menores

--- app/app_controller.php
+++ app/app_controller.php
@@ -9,14 +9,19 @@ class AppController extends Controller {
 	var $components = array('Auth', 'Session');
 	
 	function beforeFilter(){
+		//Configura el modulo de autentificación
 		$this->Auth->userModel = 'Usuario';
 		$this->Auth->fields = array('username' => 'mat_eco','password' => 'contrasena');
+		$this->Auth->logoutRedirect = '/';
+		$this->Auth->loginRedirect = '/';
+		$this->Auth->loginError = "Usuario o contraseña incorrecta";
+		$this->Auth->authError = "Debes ingresar para poder continuar";
 		
 		//Autoriza todas las páginas estáticas
 		$this->Auth->allow('display');
 		
-		//Verifica si el usuario está baneado
-		if($this->Auth->user('baneado') == 1 and $this->params['url']['url'] != 'pages/ban'){
+		//Si el usuario está baneado lo lleva a una página de advertencia (a menos que ya está ahí o vaya a salir)
+		if($this->Auth->user('baneado') == 1 and $this->params['url']['url'] != 'pages/ban' and $this->params['url']['url'] != 'usuarios/logout'){
 			$this->redirect('/pages/ban');
 		}
 	}
