86d78d2d5d3c0ed192e6f1bbc3a72a0be216899f
sexap <sexapuam@gmail.com>
2011-06-22 16:11:23 -0700

    Edited app/models/problema.php via GitHub

--- app/models/problema.php
+++ app/models/problema.php
@@ -10,5 +10,33 @@ class Problema extends AppModel {
 	var $hasMany = array('Envio', 'Comentario');
 	
 	//Validación
+        var $validate = array(
+		'titulo' => array(
+			'rule' => 'notEmpty',
+			'message' => 'Escriba el titulo del problema.'), 
+		'enunciado' => array(
+			'rule' => 'notEmpty',
+			'message' => 'Escriba el enunciado del problema.'), 
+		'autor' => array(
+			'rule' => 'notEmpty',
+			'message' => 'Se requiere el nombre del autor.'), 
+		'dificultad' => array(
+			'ruleName' => array(
+				'rule' => array('comparison', '>=', 0),
+				'message' => 'Solo se permiten enteros entre 1 y 3.'
+			),
+			'ruleName2' => array(
+				'rule' => array('comparison', '<=', 3),
+				'message' => 'Solo se permiten enteros entre 1 y 3.'
+			),
+			'ruleName3' => array(
+				'rule' => 'Numeric',
+				'message' => 'Solo se permiten enteros entre 1 y 3.'
+			)
+		),			
+		'palabras_clave' => array(
+			'rule' => 'notEmpty',
+			'message' => 'Es necesario agregar etiquetas.')
+                );
 }
 ?>
