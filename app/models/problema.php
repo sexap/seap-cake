7f28a42126ae0ee3ee3e67627d6b75d0d71c56f2
Rafael <al208337005@alumnos.azc.uam.mx>
2011-06-24 00:04:50 -0500

    Modificaciones en las validaciones del modelo de problema.

--- app/models/problema.php
+++ app/models/problema.php
@@ -12,31 +12,63 @@ class Problema extends AppModel {
 	//Validación
         var $validate = array(
 		'titulo' => array(
-			'rule' => 'notEmpty',
-			'message' => 'Escriba el titulo del problema.'), 
+			'reglatitulo-1' => array(
+				'rule' => 'alphaNumeric',
+				'message' => 'Solo se permiten letras y numeros en el titulo del problema'
+			),
+			'reglatitulo-2' => array(
+				'rule' => array('maxLength', 100),
+				'message' => 'El titulo del problema sobrepasa el limite de caracteres permitidos'
+			),
+			'reglatitulo-3' => array(
+				'rule' => 'notEmpty',
+				'message' => 'Escriba el titulo del problema.'
+			)
+		),
 		'enunciado' => array(
-			'rule' => 'notEmpty',
-			'message' => 'Escriba el enunciado del problema.'), 
+			'reglaenunciado-1' => array(
+				'rule' => array('maxLength', 500),
+				'message' => 'El enunciado del problema sobrepasa el limite de caracteres permitidos'
+			),
+			'reglaenunciado-2' => array(
+				'rule' => 'notEmpty',
+				'message' => 'Escriba el enunciado del problema.'
+			)
+		),
 		'autor' => array(
-			'rule' => 'notEmpty',
-			'message' => 'Se requiere el nombre del autor.'), 
+			'reglaautor-1' => array(
+				'rule' => 'alphaNumeric',
+				'message' => 'Solo se permiten letras y numeros en el nombre del autor del problema'
+			),
+			'reglaautor-2' => array(
+				'rule' => array('maxLength', 50),
+				'message' => 'El nombre del autor del problema sobrepasa el limite de caracteres permitidos'
+			),
+			'reglaautor-3' => array(
+				'rule' => 'notEmpty',
+				'message' => 'Escriba el nombre del autor del problema.'
+			)
+		), 
 		'dificultad' => array(
-			'ruleName' => array(
-				'rule' => array('comparison', '>=', 0),
-				'message' => 'Solo se permiten enteros entre 1 y 3.'
+			'regladificultad-1' => array(
+				'rule' => array('between', 1, 3),
+				'message' => 'La dificultad debe estar entre 1 y 3.'
 			),
-			'ruleName2' => array(
-				'rule' => array('comparison', '<=', 3),
-				'message' => 'Solo se permiten enteros entre 1 y 3.'
-			),
-			'ruleName3' => array(
+			'regladificultad-2' => array(
 				'rule' => 'Numeric',
-				'message' => 'Solo se permiten enteros entre 1 y 3.'
+				'message' => 'El valor de la dificultad debe ser numerico.'
 			)
 		),			
 		'palabras_clave' => array(
-			'rule' => 'notEmpty',
-			'message' => 'Es necesario agregar etiquetas.')
-                );
+			'reglapalabrasclave-1' => array(
+				'rule' => array('maxLength', 100),
+				'message' => 'Las palabras clave del problema sobrepasan el limite de caracteres permitidos'
+			),
+			'reglapalabrasclave-2' => array(
+				'rule' => 'notEmpty',
+				'message' => 'Escriba las palabras clave del problema.'
+			)
+		)
+	);
 }
 ?>
