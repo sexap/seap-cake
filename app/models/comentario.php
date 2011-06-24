d44194d5ba2efa8bb26f92427201b0c62fcbd801
Rafael <al208337005@alumnos.azc.uam.mx>
2011-06-23 23:30:42 -0500

    Validaciones del modelo de Comentario.

--- app/models/comentario.php
+++ app/models/comentario.php
@@ -7,5 +7,17 @@ class Comentario extends AppModel {
 	var $belongsTo = array('Usuario', 'Problema');
 	
 	//Validación
+	var $validate = array(
+		'mensaje' => array(
+			'reglamensaje-1' => array(
+				'rule' => array('maxLength', 1000),
+				'message' => 'El comentario sobrepasa el limite de caracteres permitidos'
+			),
+			'reglamensaje-2' => array(
+				'rule' => 'notEmpty',
+				'message' => 'No puede dejar un comentario en blanco.'
+			)
+		)
+	);
 }
 ?>
