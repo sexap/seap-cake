eb1ec80d8f46930af397187d29615457773c62f3
Ernesto <sirlancecc@hotmail.com>
2011-06-24 14:28:26 -0500

    pequeunio bug

--- app/app_controller.php
+++ app/app_controller.php
@@ -20,14 +20,15 @@ class AppController extends Controller {
 		$this->Auth->logoutRedirect = '/';
 		$this->Auth->loginRedirect = '/';
 		$this->Auth->loginError = 'Usuario o contraseña incorrecta';
-		$this->Auth->authError = 'No tienes permisos suficientes';
+		$this->Auth->authError = 'Para continuar debes ingresar';
 		
 		//Autoriza todas las páginas estáticas
-		$this->Auth->allow('display', 'login');
+		$this->Auth->allow('display');
 		
 		//Si el usuario está baneado lo lleva a una página de advertencia (a menos que ya está ahí o vaya a salir)
 		if($this->Auth->user('baneado') == 1 and $this->params['url']['url'] != 'pages/ban' and $this->params['url']['url'] != 'usuarios/logout'){
 			$this->redirect('/pages/ban');
+			exit();
 		}
 	}
 	
@@ -66,7 +67,7 @@ class AppController extends Controller {
 			$tabla = $modeloPermiso->find('all', $opciones);
 			//Contruir la caché de permisos
 			foreach($tabla as $renglon){
-				$aux = explode('.', $renglon['Permiso']['permiso']);
+				$aux = explode('.', low($renglon['Permiso']['permiso']));
 				$permisos[$aux[0]][$aux[1]] = 1;
 			}			
 			//Guardar los permisos en caché
@@ -83,6 +84,7 @@ class AppController extends Controller {
 		if(isset($permisos['*'][$accion])) return true;
 		if(isset($permisos[$controlador][$accion])) return true;
 		//No tiene permisos
+		//$this->Session->setFlash($this->Auth->loginError, 'default', array(), 'auth');
 		return false;
 	}
 	
