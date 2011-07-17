92c5b316f42a4ce77020dca324035578353101b4
sexap <sexapuam@gmail.com>
2011-07-17 15:54:49 -0700

    Validación para letras... =Arturo=

--- app/models/uea.php
+++ app/models/uea.php
@@ -14,10 +14,14 @@ class Uea extends AppModel {
 				'rule' => array('maxLength', 50),
 				'message' => 'El nombre de la UEA sobrepasa el limite de caracteres permitidos.'
 			),
-			'reglanuea-2' => array(
+			'reglauea-2' => array(
 				'rule' => 'notEmpty',
 				'message' => 'No puede dejar el nombre de la UEA en blanco.'
-			)
+			),
+                        'reglauea-3' => array(
+                                'rule' => '/^[a-zA-Z]$/i',
+                                'message' => 'Solo se permiten letras.'
+                        )
 		)
 	);
 }
